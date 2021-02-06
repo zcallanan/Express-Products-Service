10ab673d9c10520e8575e80ad40193076af17741
Zach Callanan <caveofmonsters@gmail.com>
2021-02-06 23:48:56 +0100

    cleanup

--- db/get-product-items.js
+++ db/get-product-items.js
@@ -1,10 +1,6 @@
 const query = require('../db/index.js');
 const format = require('pg-format');
 
-const queryP = async (string) => {
-  return await query(string);
-}
-
 const getProductItems = async (req, res, next) => {
   console.log('header is', req.header('Product'))
   let queryString = format('SELECT * FROM %I', req.header('Product'));
