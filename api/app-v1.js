309023d8a2684c64ad3b79397b578e0d9b948b4d
Zach Callanan <caveofmonsters@gmail.com>
2021-02-09 23:17:36 +0100

    Headers uppercased

--- api/app-v1.js
+++ api/app-v1.js
@@ -2,12 +2,12 @@ const request = require('request');
 // Serves as a web proxy between third party product APIs and client
 
 const appV1 = (req, res) => {
-  var targetURL = req.header('Target-URL');
+  var targetURL = req.header('X-TARGET-URL');
   if (!targetURL) {
     res.status(500).send( { error: 'There is no Target-Endpoint header in the request' });
     return;
   }
-  request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+  request({ url: targetURL + req.url, method: req.method, json: req.body },
     function (error, response, body) {
       if (error) {
         console.error('error: ' + response.statusCode)
