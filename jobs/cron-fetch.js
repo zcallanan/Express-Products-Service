e691247fb148e1cd63dc1258591fd7298c61e24e
Zach Callanan <caveofmonsters@gmail.com>
2021-02-09 20:53:35 +0100

    Added logging and env variables to enable/disable the cron

--- jobs/cron-fetch.js
+++ jobs/cron-fetch.js
@@ -6,13 +6,27 @@ if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
 }
 
-const cronFetch = () => {
+
   const task = cron.schedule(`*/${process.env.CRON_IN_MINUTES} * * * *`, () => {
     const products = ['beanies', 'facemasks', 'gloves'];
     products.forEach(product => {
       fetchProducts(product);
     })
+  }, {
+    scheduled: false
   })
-}
+
+  const cronFetch = () => {
+    if (process.env.START_CRON) {
+      console.log('start cron');
+      task.start();
+    }
+    if (process.env.END_CRON) {
+      console.log('end cron');
+      task.end();
+    }
+  }
+
+
 
 module.exports = cronFetch;
