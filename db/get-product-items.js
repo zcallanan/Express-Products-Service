c4621e3fecd78d67423eb6f025fe8812f768e90e
Zach Callanan <caveofmonsters@gmail.com>
2021-02-09 22:20:17 +0100

    removed next from getProductItems

--- db/get-product-items.js
+++ db/get-product-items.js
@@ -1,7 +1,7 @@
 const query = require('./index.js');
 const format = require('pg-format');
 
-const getProductItems = async (req, res, next) => {
+const getProductItems = async (req, res) => {
   // console.log('header is', req.header('Product'));
   let queryString = format('SELECT * FROM %I', req.header('Product'));
   const result = await query(queryString);
