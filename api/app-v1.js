95c64f6ea8ebacbc961e76f132027cd2e08baefe
Zach Callanan <caveofmonsters@gmail.com>
2021-02-09 11:22:31 +0100

    Changed color string dash to comma

--- api/app-v1.js
+++ api/app-v1.js
@@ -1,7 +1,8 @@
 const request = require('request');
+// Serves as a web proxy between third party product APIs and client
 
 const appV1 = (req, res) => {
-  var targetURL = req.header('Target-URL'); // Target-URL ie. https://example.com or http://example.
+  var targetURL = req.header('Target-URL');
   if (!targetURL) {
     res.status(500).send( { error: 'There is no Target-Endpoint header in the request' });
     return;
